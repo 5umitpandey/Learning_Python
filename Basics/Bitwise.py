fe4073edbbc4aeb638f64f6987e9fe58a4d360bd
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2024-09-07 23:19:30 +0530

    Update Bitwise.py

--- Basics/Bitwise.py
+++ Basics/Bitwise.py
@@ -31,3 +31,13 @@ print('\n')
 print(a << 1)  #0110 << 1 = 1100 = 12
 print(c << 1)  #0100 << 1 = 1000 = 8
 print('\n')
+
+
+#10: 1010
+# 4: 0100
+print( 10 & 4 )     #AND &  1,1 = 1 else 0
+print( 10 | 4 )     #OR |   1 = 1,  else 0
+print( 10 ^ 4 )     #XOR ^  1,0 = 1 else 0
+print( ~10 )        #NOT ~  1010 + 1 = 1011 = -11 (2's Complement)
+print( 10 << 2 )    #LEFT SHIFT <<  1010 = 101000 = 40
+print( 10 >> 2 )    #RIGHT SHIFT >> 1010 = 10     = 2
