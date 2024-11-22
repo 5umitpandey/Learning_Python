0b0de5763f56c0dcbe4523b6b9d942e20492e6ea
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2024-11-22 18:41:00 +0530

    Create IsPrime.py

---
+++ DSA/IsPrime.py
@@ -0,0 +1,8 @@
+def isPrime(self, num):
+        # code here
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
